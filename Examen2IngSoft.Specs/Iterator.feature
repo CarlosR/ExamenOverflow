d006c88f477f895a2ffdd9a1de61fe26f4afcefd
Jesus Mendoza <allstarmagic@hotmail.com>
2015-03-28 18:14:28 -0600

    Iterator

---
+++ Examen2IngSoft.Specs/Iterator.feature
@@ -0,0 +1,8 @@
+﻿Feature: Iterator
+
+@mytag
+Scenario: HappyPath
+	Given the result of an addition is 4
+	And the result of a substraction is -1
+	And the result of an multiplication is 12
+	Then the results in the log screen should be 'Suma 4', 'Resta -1', 'Multiplicacion 12'
--- Examen2IngSoft.Specs/IteratorSteps.cs
+++ Examen2IngSoft.Specs/IteratorSteps.cs
@@ -9,38 +9,40 @@ using System.Linq;
 namespace Examen2IngSoft.Specs
 {
     [Binding]
-    public class DecoratorPatternSteps
+    public class IteratorSteps
     {
-        private int _additionResult = 0;
-        private int _substractionResult = 0;
-        private int _multiplicationResult = 0;
+        private int _infoAddition = new Addition();
+        private int _infoSubstraction = new Substraction();
+        private int _infoMultiplication = new Multiplication();
         private ILog log;
         private IFileWriter _fileWriter;
         private ICreator _creator;
-
+        
         [Given(@"the result of an addition is (.*)")]
         public void GivenTheResultOfAnAdditionIs(int p0)
         {
-            _additionResult = p0;
+            _infoAddition(p0);
         }
         
         [Given(@"the result of a substraction is (.*)")]
         public void GivenTheResultOfASubstractionIs(int p1)
         {
-            _substractionResult = p1;
+            _infoSubstraction(p1);
         }
         
         [Given(@"the result of an multiplication is (.*)")]
         public void GivenTheResultOfAnMultiplicationIs(int p2)
         {
-            _multiplicationResult = p2;
+            _infoMultiplication(p2);
         }
 
-        [Then(@"the log string should be '(.*)', '(.*)', '(.*)'")]
-        public void ThenTheLogStringShouldBe(string p0, string p1, string p2)
+        [Then(@"the results in the log screen should be '(.*)', '(.*)', '(.*)'")]
+        public void ThenTheResultsInTheLogScreenShouldBe()
         {
-            _creator = new MultiplicationCreator();
-            log = new Multiplication(new Substraction(new Addition(new BaseLog(),_additionResult), _substractionResult), _multiplicationResult);
+            _creatorAdd = new AdditionCreator();
+            _creatorSub = new SubstractionCreator();
+            _creatorMult = new MultiplicationCreator();
+            log = new Multiplication(new Substraction(new Addition(new BaseLog(), _infoAddition), _infoSubstraction), _infoMultiplication);
             List<string> resultList = log.GetLog();
             _fileWriter = new FileWriter();
             _fileWriter.WriteLog(resultList);
